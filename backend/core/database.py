2e05d3708ab9ec1fbaf8583705fe59b21001584a
FaizaanAlFaisal <101617018+FaizaanAlFaisal@users.noreply.github.com>
2025-05-23 12:41:46 +0500

    added: database setup

---
+++ backend/core/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import settings
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+from models import Base
+Base.metadata.create_all(bind=engine)
+logger.info("✅ Database tables created successfully. ✅")
+
+def test_db():
+    try:
+        db = SessionLocal()
+        db.execute("SELECT 1")
+        logger.info("Database connection successful.")
+    except Exception as e:
+        logger.error(f"Database connection failed: {e}")
+    finally:
+        db.close()
