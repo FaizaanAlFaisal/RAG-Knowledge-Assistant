2c5b6c146ab6c729ba80e0ef7455501e25afd899
FaizaanAlFaisal <101617018+FaizaanAlFaisal@users.noreply.github.com>
2025-05-26 10:32:53 +0500

    dynamic db/redis url generation

--- backend/core/config.py
+++ backend/core/config.py
@@ -7,13 +7,19 @@ class Settings(BaseSettings):
     DB_NAME: str = Field(..., env="DB_NAME")
     DB_USER: str = Field(..., env="DB_USER")
     DB_PASSWORD: str = Field(..., env="DB_PASSWORD")
-    DB_URL: str = f"{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+    
+    @property
+    def DB_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
     # Redis settings
     REDIS_HOST: str = Field(..., env="REDIS_HOST")
     REDIS_PORT: int = Field(..., env="REDIS_PORT")
     REDIS_DB: int = Field(..., env="REDIS_DB")
-    REDIS_URL: str = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+    
+    @property
+    def REDIS_URL(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
 
     # Other settings
     SECRET_KEY: str = Field(..., env="SECRET_KEY")
